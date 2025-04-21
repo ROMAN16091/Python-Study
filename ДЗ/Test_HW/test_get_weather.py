178e6ade61572a9ad338a3a5727c944ad0bf0cf2
ROMAN16091 <roma.medvednikov@gmail.com>
2025-04-21 17:34:34 +0300

    Added info for studying

--- ДЗ/Test_HW/test_get_weather.py
+++ ДЗ/Test_HW/test_get_weather.py
@@ -1,22 +1,25 @@
 import requests, json
 url = requests.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=5c62da02e9e30f91537def5669c10abc&units=metric&lang=uk')
 
-def get_weather_from_api(data):
+def get_weather_from_api(data, status_code):
     temp = data['main']['temp']
     description = data['weather'][0]['description']
-    if url.status_code == 200:
+    if status_code == 200:
         return {
             "temp": temp,
             "description": description
-        }
+        }, status_code
     else:
-        return url.status_code
+        return status_code
 
 
 def test_get_weather_from_api():
-    weather_info = {"temp": 16.1, "description": 'рвані хмари'}
-
-    assert get_weather_from_api(url.json()) == weather_info, 200
+    expected = ({"temp": 16.09, "description": 'рвані хмари'}, 200)
+    actual = {
+        "main": {"temp": 16.09},
+        "weather": [{"description": "рвані хмари"}]
+    }
+    assert get_weather_from_api(actual, 200) == expected
 
 if __name__ == '__main__':
     test_get_weather_from_api()
